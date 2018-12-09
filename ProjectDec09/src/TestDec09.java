a72ab681345b27f42f142a2a948539f9ed1ba0ec
Jose94212 <jose.g.payyappilly@gmail.com>
2018-12-09 14:24:03 +0530

    "added a variable"

--- ProjectDec09/src/TestDec09.java
+++ ProjectDec09/src/TestDec09.java
@@ -1,9 +1,10 @@
 
 public class TestDec09 {
-
+	static String AGC= "another environment";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi, i am created on Dec 09");
+		System.out.println(AGC);
 
 	}
 
